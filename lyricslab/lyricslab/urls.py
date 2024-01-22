ac63251206d35530ca93531efae1528f3a965843
axelthereal <mail.axeltokam@gmail.com>
2024-01-22 13:40:13 +0100

    edited and added new python project files

--- lyricslab/lyricslab/urls.py
+++ lyricslab/lyricslab/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import views
 
 urlpatterns = [
+    # custom
+
+    # default
     path('admin/', admin.site.urls),
 ]
