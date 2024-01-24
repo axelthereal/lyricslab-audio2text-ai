0069d70b8dacf1fd637c89482296e4bba9025ced
axelthereal <mail.axeltokam@gmail.com>
2024-01-24 11:19:32 +0100

    modified views.py

--- lyricslab/app/urls.py
+++ lyricslab/app/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     # custom
     path('', home_view, name="default_pages_view"),
     path('home', home_view, name="home_view"), 
+
+    # api_routes
+    path('appnode/v1.0.1/runtime/upload', upload_media, name="media_upload"),
 ]
--- lyricslab/app/views.py
+++ lyricslab/app/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Media
 def home_view(req):
     ctx = {
         "view_name": "Home"
     }
     return render(req, 'pages/home.html', ctx)
  
- 
+
+# handles_forms_uploads
+def upload_media(req):
+    if req.method == 'POST' and req.FILES['fileObj']:
+        fileObj = req.FILES['fileObj']
+        filetitle = req.FILES['fileTitle']
+        instance = Media(title=filetitle, media_file=fileObj)
+        instance.save()
+        return JsonResponse({
+        "status": "success",
+        "msg": "File Uploaded Successfully"
+    })
+
+    return JsonResponse({
+        "status": "error",
+        "msg": "File Not Found"
+    }) 
+
+    
