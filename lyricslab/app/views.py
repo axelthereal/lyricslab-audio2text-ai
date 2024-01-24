93e8260e8c7fac5aab5ab48cd8358ca7496d442e
axelthereal <mail.axeltokam@gmail.com>
2024-01-24 11:24:18 +0100

    minor changes to views.py

--- lyricslab/app/views.py
+++ lyricslab/app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render 
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from .models import Media
 def home_view(req):
     ctx = {
